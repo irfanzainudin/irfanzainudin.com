1abf765c4c68fa77d6c708ba7b29760c172b2a06
irfanzainudin <m.irfan.zain@gmail.com>
2025-04-15 12:40:18 +0800

    added 2d-portfolio

---
+++ 2d-portfolio/README.md
@@ -0,0 +1,7 @@
+# 2d Portfolio
+
+
+
+## Acknowledgement
+
+- [Build a Dev Portfolio as a 2D Game – JavaScript Course](https://www.youtube.com/watch?v=wy_fSStEgMs)
---
+++ 2d-portfolio/src/utils.js
@@ -0,0 +1,48 @@
+export function displayDialogue(text, onDisplayEnd) {
+  // Logic for displaying dialogueUI
+  const dialogueUI = document.getElementById("textbox-container");
+  const dialogue = document.getElementById("dialogue");
+
+  dialogueUI.style.display = "block";
+
+  let index = 0;
+  let currentText = "";
+  const intervalRef = setInterval(() => {
+    if (index < text.length) {
+      currentText += text[index];
+      dialogue.innerHTML = currentText;
+      index++;
+      return;
+    }
+
+    clearInterval(intervalRef);
+  }, 1);
+
+  // Logic for closing dialogueUI
+  const closeBtn = document.getElementById("close");
+
+  function onCloseBtnClick() {
+    onDisplayEnd();
+    dialogueUI.style.display = "none";
+    dialogue.innerHTML = "";
+    clearInterval(intervalRef);
+    closeBtn.removeEventListener("click", onCloseBtnClick);
+  }
+
+  closeBtn.addEventListener("click", onCloseBtnClick);
+
+  addEventListener("keypress", (key) => {
+    if (key.code === "Enter") {
+      closeBtn.click();
+    }
+  });
+}
+
+export function setCamScale(k) {
+  const resizeFactor = k.width() / k.height();
+  if (resizeFactor < 1) {
+    k.camScale(k.vec2(1));
+  } else {
+    k.camScale(k.vec2(1.5));
+  }
+}
